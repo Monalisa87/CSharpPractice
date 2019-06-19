ebc7738b4cd461fd2fa3ff6cff98f1602e19185f
Monalisa <monalisa.panda1@gmail.com>
2019-06-19 09:45:37 +0530

    Find minimum missing integer from array

---
+++ MissingInteger/MissingInteger/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MissingInteger
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] arrayValue = new int[] { 3, 4, 7, 6 };
+			Console.WriteLine(FindMinimumIntegerMissing(arrayValue));
+			Console.ReadLine();
+		}
+		static int FindMinimumIntegerMissing(int[] arrayInt)
+		{
+			var arrayMin = arrayInt.Min();
+			var arrayMax = arrayInt.Max();
+
+			for (int value = arrayMin; value < arrayMax; value++)
+			{
+				if (!arrayInt.OrderByDescending(a => a).Contains(value))
+				{
+					return value;
+				}
+
+			}
+			return arrayMax + 1;
+
+		}
+	}
+}
