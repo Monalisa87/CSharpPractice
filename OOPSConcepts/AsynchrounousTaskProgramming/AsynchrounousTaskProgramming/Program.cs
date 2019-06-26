d60ebc32ddb2669985bd29c391922d35eb412e25
Monalisa <monalisa.panda1@gmail.com>
2019-06-26 06:58:08 +0530

    Asynchonous Task Programming STart Task and Task Wait

---
+++ OOPSConcepts/AsynchrounousTaskProgramming/AsynchrounousTaskProgramming/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsynchrounousTaskProgramming
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Thread.CurrentThread.Name = "Main";
+
+			//// Create a task and supply a user delegate by using a lambda expression. 
+			//Task taskA = new Task(() => Console.WriteLine("Hello from taskA."));
+			//// Start the task.
+			//taskA.Start();
+
+			// Define and run the task.
+			Task taskA = Task.Run(() => Console.WriteLine("Hello from taskA."));
+
+			
+
+			// Output a message from the calling thread.
+			Console.WriteLine("Hello from thread '{0}'.",
+							  Thread.CurrentThread.Name);
+			taskA.Wait();
+			Console.ReadKey();
+		}
+	}
+}
