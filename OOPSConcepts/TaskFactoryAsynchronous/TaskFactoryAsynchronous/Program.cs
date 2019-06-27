d83b0bb304cc5d2edcebeaf9cd2cfdce9719cccb
Monalisa <monalisa.panda1@gmail.com>
2019-06-27 09:51:55 +0530

    Asynchronous task programming

---
+++ OOPSConcepts/TaskFactoryAsynchronous/TaskFactoryAsynchronous/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskFactoryAsynchronous
+{
+	class CustomData
+	{
+		public long CreationTime;
+		public int Name;
+		public int ThreadNum;
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Task[] taskArray = new Task[10];
+			for (int i = 0; i < taskArray.Length; i++)
+			{
+				taskArray[i] = Task.Factory.StartNew((Object obj) => {
+					CustomData data = obj as CustomData;
+					if (data == null)
+						return;
+
+					data.ThreadNum = Thread.CurrentThread.ManagedThreadId;
+				},
+													  new CustomData() { Name = i, CreationTime = DateTime.Now.Ticks });
+			}
+			Task.WaitAll(taskArray);
+			foreach (var task in taskArray)
+			{
+				var data = task.AsyncState as CustomData;
+				if (data != null)
+					Console.WriteLine("Task #{0} created at {1}, ran on thread #{2}.",
+									  data.Name, data.CreationTime, data.ThreadNum);
+			}
+			Console.ReadKey();
+		}
+	}
+}
